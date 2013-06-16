b9100187b1282a651cae141fe5075ac4a84c42a4
Zamith <zamith.28@gmail.com>
2013-06-16 12:16:34 +0100

    Creates gem

---
+++ lib/fake/paymill.rb
@@ -0,0 +1,7 @@
+require "fake/paymill/version"
+
+module Fake
+  module Paymill
+    # Your code goes here...
+  end
+end
---
+++ lib/fake/paymill/version.rb
@@ -0,0 +1,5 @@
+module Fake
+  module Paymill
+    VERSION = "0.0.1"
+  end
+end
